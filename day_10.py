311e8b008cc794ef8a0a63713ae4a099416c1b8a
Urs Dannenberg <urs.dannenberg@emsysgrid.de>
2025-12-19 13:44:54 +0100

    Add solutions for first part of day 10.

---
+++ day_10.py
@@ -0,0 +1,35 @@
+import itertools
+
+
+def _reader(filename):
+    with open(filename, "r") as file:
+        machines = []
+        for line in file:
+            lights_, rest = line.strip("[").split("] (")
+            lights = tuple(int(char == "#") for char in lights_)
+            buttons_, joltages_ = rest.strip("}\n").split(") {")
+            buttons = {
+                tuple(int(light) for light in button.split(","))
+                for button in buttons_.split(") (")
+            }
+            joltages = tuple(int(joltage) for joltage in joltages_.split(","))
+            machines.append((lights, buttons, joltages))
+    return machines
+
+
+def solver(filename) -> None:
+    machines = _reader(filename)
+    part_1, part_2 = 0, 0
+    for lights, buttons, joltages in machines:
+        for i, combination in (
+            (i_, combination_)
+            for i_ in range(len(buttons))
+            for combination_ in itertools.combinations(buttons, i_)
+        ):
+            if lights == tuple(
+                sum(int(i in button) for button in combination) % 2
+                for i in range(len(lights))
+            ):
+                part_1 += i
+                break
+    print(f"Part 1: {part_1}")
